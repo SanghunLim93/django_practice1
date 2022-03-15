fb093fa5ba7def8846f0e489ca27d8c1aa303761
Sanghun Lim <sanghunlim1993@gmail.com>
2022-03-15 12:24:01 +0900

    첨부 파일 잇을 경우 포스트 상세 페이지에 다운로드 버튼 출력하기

--- blog/models.py
+++ blog/models.py
@@ -1,6 +1,5 @@
 from django.db import models
-
-from do_it_django_prj import settings
+import os
 
 
 class Post(models.Model):
@@ -8,10 +7,10 @@ class Post(models.Model):
     content = models.TextField()
 
     head_image = models.ImageField(upload_to='blog/images/%Y/%m/%d/', blank=True)
-    file_upload = models.FileField(upload_to='blog/files/%y/%m/%d/', blank=True)
+    file_upload = models.FileField(upload_to='blog/files/%Y/%m/%d/', blank=True)
+
     created_at = models.DateTimeField(auto_now_add=True)
-    Update_At = models.DateTimeField(auto_now=True)
-    #author: 추후 작성 예정
+    update_at = models.DateTimeField(auto_now=True)
 
     def __str__(self):
         return f'[{self.pk}]{self.title}'
@@ -19,5 +18,8 @@ class Post(models.Model):
     def get_absolute_url(self):
         return f'/blog/{self.pk}/'
 
-     
-# Create your models here.
+    def get_file_name(self):
+        return os.path.basename(self.file_upload.name)
+
+    def get_file_ext(self):
+        return self.get_file_name().split('.')[-1]
